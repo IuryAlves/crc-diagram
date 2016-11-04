71b9c012550dbffdee7f81c918c0f03b9967c56b
iury <satriani-16@hotmail.com>
2016-11-04 01:03:14 -0200

    refactor parser

--- pycrc/core/parser.py
+++ pycrc/core/parser.py
@@ -31,22 +31,27 @@ class CRCParser(ast.NodeVisitor):
             "classes": {crc.name: crc.to_dict() for crc in self._crcs}
         }
 
-    def _get_responsibility(self, string):
-        responsibility_match = self.responsibility_pattern.match(string)
-        if responsibility_match is not None:
-            responsibility = responsibility_match.group(1).strip()
+    def get_responsibility(self, string):
+        responsibility = self._get_annotation(
+            string,
+            self.responsibility_pattern
+        )
+        if responsibility:
             self.current_crc.responsibilities.append(responsibility)
 
-    def _get_collaborator(self, string):
-        collaborator_match = self.collaborator_pattern.match(string)
-        if collaborator_match is not None:
-            collaborator = collaborator_match.group(1).strip()
+    def get_collaborator(self, string):
+        collaborator = self._get_annotation(string, self.collaborator_pattern)
+        if collaborator:
             self.current_crc.collaborators.append(collaborator)
+
+    def _get_annotation(self, string, pattern):
+        annotation = pattern.match(string)
+        return annotation.group(1).strip() if annotation is not None else None
 
     def visit_ClassDef(self, node):
         self.current_crc = self.CRC(name=node.name)
         docstring = ast.get_docstring(node) or ""
         for line in docstring.split('\n'):
-            self._get_collaborator(line)
-            self._get_responsibility(line)
+            self.get_collaborator(line)
+            self.get_responsibility(line)
         self._crcs.append(self.current_crc)
