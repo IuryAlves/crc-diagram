91caac99a439f399b93b2c15f8943fb3cbc65b14
IuryAlves <satriani-16@hotmail.com>
2016-12-17 22:03:40 -0200

    fixing build

--- crc_diagram/__main__.py
+++ crc_diagram/__main__.py
@@ -5,36 +5,34 @@ from __future__ import (
     unicode_literals
 )
 
-import argparse
 import json
 import sys
 import os
 from itertools import chain
+import click
 import crc_diagram
 from crc_diagram.renders import RenderTo
 
 
-def _create_parser():
-    parser = argparse.ArgumentParser(description='Generate CRC diagrams from python code.')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--raw', type=bool)
-    group.add_argument('--render', default='svg', type=str)
-    parser.add_argument('source')
-    parser.add_argument('out')
-    return parser
+@click.command(name='pycrc')
+@click.argument('source')
+@click.argument('out', required=False)
+@click.option('--raw', type=bool)
+@click.option('--render', default='svg', type=str)
+def main(source, out, raw, render):
 
-
-if __name__ == '__main__':
-    args = _create_parser().parse_args()
-    source = args.source
-    out = args.out
     if os.path.isdir(source):
         crc_cards = chain.from_iterable(crc_diagram.project_to_crc(source))
     else:
         crc_cards = crc_diagram.py_to_crc(source)
 
-    if args.raw:
+    if raw:
         out = out or sys.stdout
         json.dump([crc.to_dict() for crc in crc_cards], out, indent=4)
     else:
-        RenderTo(args.render, 0, 0, 150, 300).draw(crc_cards, out)
+        if out is None:
+            raise click.UsageError('Missing argument "out".')
+        RenderTo(render, 0, 0, 150, 300).draw(crc_cards, out)
+
+if __name__ == '__main__':
+    main()
--- tests/command_line_tests.py
+++ tests/command_line_tests.py
@@ -8,34 +8,34 @@ from __future__ import (
 import unittest
 import subprocess
 import json
+import os
 from crc_diagram import test
 
 
 class CommandLineTestCase(test.CrcTestCase):
 
     def test_render_crc_cards_as_svg(self):
-        command = 'python -m crc_diagram --render=svg ' \
-                  '{test_files}/{folder} test.svg'.format(
-                    test_files=self.test_files,
-                    folder='project'
+        command = 'python -m crc_diagram --render=svg {test_files}/{folder} test.svg'.format(
+            test_files=self.test_files,
+            folder='project'
         )
         status = subprocess.check_call(command.split())
 
         self.assertEqual(status, 0)
 
     def test_execute_as_raw(self):
-        command = 'python -m crc_diagram --raw=true {test_files}/{file}'.format(
-            test_files=self.test_files,
-            file='class_collaborator',
+        command = 'python -m crc_diagram --raw=true {folder}/{file}'.format(
+            folder=os.path.join(self.test_files, 'project'),
+            file='professor.py',
         )
         out = subprocess.check_output(command.split())
         result = json.loads(out.decode())
         self.assertEqual(result,
                          [
                              {
-                                 'name': 'HtmlParser',
-                                 'collaborators': ['ImageUploader'],
-                                 'responsibilities': []
+                                 'name': 'Professor',
+                                 'collaborators': ['Seminar'],
+                                 'responsibilities': ['Provide information', 'Get Seminars instructing']
                              }
                          ])
 
