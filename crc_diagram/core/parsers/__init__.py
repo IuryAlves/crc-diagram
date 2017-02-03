d46a1d764efee598feff9bef74655fa8b58622e5
IuryAlves <satriani-16@hotmail.com>
2017-02-02 22:09:00 -0200

    removing smalltalk parser. It will be reimplemented later

--- crc_diagram/core/parsers/__init__.py
+++ crc_diagram/core/parsers/__init__.py
@@ -2,12 +2,10 @@
 
 
 from .python_parser import PythonParser
-from .smalltalk_parser import SmallTalkParser
 from .base_parser import BaseParser
 
 
 __all__ = [
     'BaseParser',
-    'SmallTalkParser',
     'PythonParser'
 ]
